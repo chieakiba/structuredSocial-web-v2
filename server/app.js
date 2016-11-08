e60817fe9d37576407a59a94a705d2053c3e3239
Chie <akiba.chie@gmail.com>
2016-11-07 22:47:20 -0800

    Tested out GET and it works. POST seems work as well. Now testing it with Send Grid

--- server/app.js
+++ server/app.js
@@ -8,31 +8,43 @@ const sg = require('sendgrid')(config.API_KEY);
 const app = express()
 
 console.log('hello')
-app.use(morgan('dev'));
+app.use(express.static('build/js'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(express.static('build/js'));
 
-app.use('*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+app.use(morgan('dev'));
+
+// app.use('*', (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
+
+app.get('/send/mail', (req, res) => {
+  res.json({
+    msg: 'Why is this not working?'
+  })
+})
 
 app.post('/send/mail', (req, res) => {
+  const email = req.body
   console.log('what is req.body', req.body)
+  res.send({
+    msg: 'does this show?',
+    email
+  })
 
   // ==============================
   // SENGRID ~ EMAIL
   // ==============================
 
-  const noReplyEmail = new helper.Email('noreply@structured-social.com');
+  const fromEmail = new helper.Email('noreply@structured-social.com');
   // const fromEmail = new helper.Email(config.email);
   const toEmail = new helper.Email(req.body.to);
   const subject = 'Hello from Structured Social!';
   const content = new helper.Content('text/plain', 'Welcome to Structured Social!');
-  const mail = new helper.Mail(noReplyEmail, toEmail, subject, content);
+  const mail = new helper.Mail(fromEmail, toEmail, subject, content);
   const request = sg.emptyRequest({
     method: 'POST',
     path: '/v3/mail/send',

cdf022dffeb4c4ef3c05f3efa882917f50e3c4ae
Chie <akiba.chie@gmail.com>
2016-11-14 09:40:22 -0800

    Added Structured Social logo in the header

--- src/components/title.js
+++ src/components/title.js
@@ -5,7 +5,7 @@ const Title = () => {
   return (
     <header id="header">
       <Row className="show-grid text-center">
-        <h1 className="companyname">Structured Social</h1>
+        <img src="/images/SocialStructure-01.png" alt="logo"/>
         <h3>Lorem ipsum dolor sit amet consectetur adipiscing elit</h3>
       </Row>
     </header>
